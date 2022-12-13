9af3991007a49ed6c32f0dd5d303f2a59b8d771f
rafalgw59 <r.gaw59lik@gmail.com>
2022-12-13 14:39:58 +0100

    Initial Commit

---
+++ AIChatter/AIChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatterApp.swift
+//  AIChatter
+//
+//  Created by Rafał Gawlik on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AIChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
