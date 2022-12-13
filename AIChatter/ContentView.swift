8003ce347ed354c1bb013d3e89c598717a4cc11f
rafalgw59 <r.gaw59lik@gmail.com>
2022-12-13 15:37:43 +0100

    Jakiś commit

--- AIChatter/ContentView.swift
+++ AIChatter/ContentView.swift
@@ -6,16 +6,71 @@
 //
 
 import SwiftUI
+import OpenAISwift
+
+final class ViewModel: ObservableObject {
+    init(){}
+    
+    private var client: OpenAISwift?
+    
+    
+    func setup(){
+            client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String, completion: @escaping (String)->Void){
+        client?.sendCompletion(with: text,
+                               maxTokens: 1000,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+        })
+    }
+}
 
 struct ContentView: View {
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self){ string in
+                Text(string)
+                
+            }
+            Spacer()
+            
+            HStack{
+                TextField("Type here...", text: $text)
+                Button("Send"){
+                    send()
+                }
+            }
+        }
+        .onAppear{
+            viewModel.setup()
         }
         .padding()
+    }
+    
+    func send(){
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else{
+            return
+        }
+        models.append("Me: \(text)")
+        viewModel.send(text: text){ response in
+            DispatchQueue.main.async{
+                self.models.append("Bot: "+response)
+                self.text = ""
+            }
+        }
     }
 }
 
